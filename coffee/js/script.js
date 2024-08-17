6686c491d19ac7f7fd1c2dddcb71b52809d20b53
DESKTOP-452D25C\User <tncnp27402@gmail.com>
2024-08-17 10:41:39 +0700

    first commit

---
+++ coffee/js/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navalist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navalist.classList.toggle("open");
+};
